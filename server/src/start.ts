43f434e97f586105eed474af2a0015ea6246d45c
MJ554 <156290608+MJ554@users.noreply.github.com>
2025-02-16 09:53:58 +0100

    print requests in backend.

--- server/src/start.ts
+++ server/src/start.ts
@@ -5,6 +5,12 @@ import cors from "cors";
 
 export const app = express();
 
+app.use((req, res, next) => {
+    const now = new Date().toISOString();
+    console.log(`[${now}] ${req.method} ${req.url}`);
+    next();
+  });
+
 app.use(express.json()); // Middleware
 app.use(cors());
 app.use("/book", bookRouter);
