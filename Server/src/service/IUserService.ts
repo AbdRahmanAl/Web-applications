511bc16f034916c0618d9b0f8ee69738a5f2a93c
Afaf Konkabess <143448788+Afaf-Konkabess@users.noreply.github.com>
2025-03-15 17:57:15 +0100

    add comments

--- Server/src/service/IUserService.ts
+++ Server/src/service/IUserService.ts
@@ -1,9 +1,12 @@
-import {User} from "../model/user";
+import {User} from "../model/user";// Importing the User model
 
 
 
 export interface IUserService {
+    // Create a new user with the given username and password
     createUser(username: string, password: string): Promise<void>;
+    // Find a user by username, with an optional password check
+    // Return the user object if found, otherwise return undefined
     findUser(username: string, password?: string): Promise<User | undefined>;
   }
   
