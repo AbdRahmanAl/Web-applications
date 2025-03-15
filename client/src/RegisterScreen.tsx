9fe371af5e19ee5563c41396a6a201fb1bdf57b3
Afaf Konkabess <143448788+Afaf-Konkabess@users.noreply.github.com>
2025-03-15 18:29:12 +0100

    add comments

--- client/src/RegisterScreen.tsx
+++ client/src/RegisterScreen.tsx
@@ -3,6 +3,7 @@ import { NavLink, useNavigate } from "react-router";
 import { registerUser } from "./api";
 
 export function RegisterScreen() {
+    // store username and password inputs
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const navigate = useNavigate();
@@ -23,8 +24,8 @@ export function RegisterScreen() {
                 }}></input>
             </p>
             <p><button onClick={async () => {
-                await registerUser(username, password);
-                navigate("/login");
+                await registerUser(username, password);// Call register function with user credentials
+                navigate("/login");// Redirect to login page after successful registration
             }}>Register </button></p>
             <NavLink to="/login" end>Back to login screen</NavLink>
         </section>
