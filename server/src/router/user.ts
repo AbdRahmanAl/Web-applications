5e99e3a497989ca21f77f1ea674b846f9976cd77
Afaf Konkabess <143448788+Afaf-Konkabess@users.noreply.github.com>
2025-03-15 20:54:05 +0100

    add comments

--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -2,8 +2,9 @@ import express, { Request, Response, Router } from "express";
 import { UserService } from "../service/user";
 import { User } from "../model/user";
 
+//Create and return a router for handling user authentication.
 export function userRouter(userService: UserService): Router {
-    const userRouter = express.Router();
+    const userRouter = express.Router();// Initialize the router
 
     interface UserRequest extends Request {
         body: { username: string, password: string },
@@ -14,11 +15,13 @@ export function userRouter(userService: UserService): Router {
         session: any
     }
 
+    //Register a new user.
     userRouter.post("/user", async (req: UserRequest, res: Response) => {
         userService.createUser(req.body.username, req.body.password);
         res.status(201).send({username: req.body.username});
     })
 
+    //Log in a user by verifying credentials
     userRouter.post("/user/login", async (req: UserRequest, res: Response) => {
         const user: User | undefined = await userService.findUser(req.body.username, req.body.password);
         if (!user) {
@@ -29,6 +32,7 @@ export function userRouter(userService: UserService): Router {
         res.status(200).send("Logged in");
     })
 
+    //Log out the current user by clearing the session.
     userRouter.post("/user/logout", async (req: LoggedUserRequest, res: Response) => {
         if (req.session.username) {
             req.session.username = undefined; // Logout
