82f44f7c35d3747a3caa004a7e85a1633b805fa8
Afaf Konkabess <143448788+Afaf-Konkabess@users.noreply.github.com>
2025-03-15 20:10:16 +0100

    add comments

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Create the root React rendering container and attach it to the 'root' element in the HTML
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
